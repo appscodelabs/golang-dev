a0a7eef9c3f041ad65a9afac891e18db950cea36
Tamal Saha <tamal@appscode.com>
2019-08-15 20:21:28 -0700

    Support Go 1.12.9

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build --pull -t appscode/golang-dev:1.12.8-alpine -f Dockerfile.alpine .
-docker push appscode/golang-dev:1.12.8-alpine
+docker build --pull -t appscode/golang-dev:1.12.9-alpine -f Dockerfile.alpine .
+docker push appscode/golang-dev:1.12.9-alpine
 
-docker build --pull -t appscode/golang-dev:1.12.8-stretch -f Dockerfile.stretch .
-docker push appscode/golang-dev:1.12.8-stretch
+docker build --pull -t appscode/golang-dev:1.12.9-stretch -f Dockerfile.stretch .
+docker push appscode/golang-dev:1.12.9-stretch
