c2cce44decb9e87771585dc7ff454070ee645494
Tamal Saha <tamal@appscode.com>
2019-10-18 15:03:46 -0700

    1.12.12

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 GO Docker image used for building AppsCode projects
 
 ```console
-docker pull appscode/golang-dev:1.12.10-stretch
-docker pull appscode/golang-dev:1.12.10-alpine
+docker pull appscode/golang-dev:1.12.12-stretch
+docker pull appscode/golang-dev:1.12.12-alpine
 ```
--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build --pull -t appscode/golang-dev:1.12.10-alpine -f Dockerfile.alpine .
-docker push appscode/golang-dev:1.12.10-alpine
+docker build --pull -t appscode/golang-dev:1.12.12-alpine -f Dockerfile.alpine .
+docker push appscode/golang-dev:1.12.12-alpine
 
-docker build --pull -t appscode/golang-dev:1.12.10-stretch -f Dockerfile.stretch .
-docker push appscode/golang-dev:1.12.10-stretch
+docker build --pull -t appscode/golang-dev:1.12.12-stretch -f Dockerfile.stretch .
+docker push appscode/golang-dev:1.12.12-stretch
