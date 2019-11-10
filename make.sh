f4e15a13afbfcf8265076acfb9a23854dc5d977e
Tamal Saha <tamal@appscode.com>
2019-11-09 18:01:36 -0800

    Only maintain Debian buster based image

--- make.sh
+++ make.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build --pull -t appscode/golang-dev:1.12.12-alpine -f Dockerfile.alpine .
-docker push appscode/golang-dev:1.12.12-alpine
-
-docker build --pull -t appscode/golang-dev:1.12.12-stretch -f Dockerfile.stretch .
-docker push appscode/golang-dev:1.12.12-stretch
-
-docker build --pull -t appscode/golang-dev:1.12.12-buster -f Dockerfile.buster .
-docker push appscode/golang-dev:1.12.12-buster
+docker build --pull -t appscode/golang-dev:1.12.12 -f Dockerfile .
+docker push appscode/golang-dev:1.12.12
