bcd7aac2642bfd24e6acb8d1eb79c1970678969e
Tamal Saha <tamal@appscode.com>
2019-12-09 13:40:32 -0800

    Go 1.13.5
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.13.4-buster
+FROM golang:1.13.5-buster
 
 RUN set -x \
   && apt-get update \
--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-docker build --pull -t appscode/golang-dev:1.13.4 -f Dockerfile .
-docker push appscode/golang-dev:1.13.4
+docker build --pull -t appscode/golang-dev:1.13.5 -f Dockerfile .
+docker push appscode/golang-dev:1.13.5
